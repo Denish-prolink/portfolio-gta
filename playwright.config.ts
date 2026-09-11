555208899293bb2c116dbbb411317085bb87f142
Denish-kunjadiya <denish.prolink@gmail.com>
2026-09-11 12:18:55 +0530

    remove env thing

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  use: { baseURL: 'http://localhost:4321', browserName: 'chromium', channel: process.env.PLAYWRIGHT_CHANNEL || undefined },
+  use: { baseURL: 'http://localhost:4321', browserName: 'chromium', channel: undefined },
   webServer: { command: 'npm run preview', url: 'http://localhost:4321', reuseExistingServer: true },
 });
